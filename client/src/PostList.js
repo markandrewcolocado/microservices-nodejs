1baeb9e9c69e3d2537c300a229bd186ff8a8843c
DESKTOP-BD1HNTD\Mark <markandrew.colocado@gmail.com>
2024-08-02 10:34:38 +0800

    Initial deployment, working services with container and kube deployments/services.

---
+++ client/src/PostList.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
+
+export default () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get("http://query-srv:4002/posts");
+    console.log(res.data);
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  // Object.values = built-in js function that gives an array of all the values inside of an object
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}>
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentList comments={post.comments} />
+          <CommentCreate postId={post.id} />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
+};
---
+++ event-bus/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const events = [];
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  // Post service
+  axios.post("http://posts-srv:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  // Comments service
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  // Query service
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  // Moderation service
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
+
+app.listen(4005, () => {
+  console.log("Listening on port 4005");
+});
